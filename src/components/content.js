30683a46a6fdc9af98058c74e13af43e95cd09d3
Ben Yu <cnhnyu@gmail.com>
2018-07-05 13:58:51 +1000

    Add button click action to dump component

--- src/components/content.js
+++ src/components/content.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 
 export default function Content(props) {
+    const showOrHide = props.showMessage ? "Hide" : "Show";
+
     return (<div>
         <h1>This is main content</h1>;
-        <h2>{props.msg}</h2>
+        <button onClick={props.handleClick} className="btn btn-primary">{showOrHide} the message</button>
+        {props.showMessage && <h2>{props.msg}</h2>}
     </div>
     );
 }
--- src/containers/main.js
+++ src/containers/main.js
@@ -7,13 +7,20 @@ class MainContainer extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            showMessage: false
+        };
+    }
+
+    onClickButton()
+    {
+        this.setState({showMessage: !this.state.showMessage});
     }
 
     render() {
         return (<div>
             <Header />
-            <Content msg='Message from MainContainer'/>
+            <Content msg='Message from MainContainer' handleClick={this.onClickButton.bind(this)} showMessage={this.state.showMessage}/>
             <Footer />
         </div>);
     }
